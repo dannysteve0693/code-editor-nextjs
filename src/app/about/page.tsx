babd661a5c2f0fc176605fd8b6dd17d47d96caba
Danny <dannysteve0693@gmail.com>
2025-10-14 18:50:32 +0700

    Added ENV Example

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    console.log('hey dari console about')
+    return (
+        <div>
+            About
+        </div>
+    )
+}
+
+export default About
